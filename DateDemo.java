6092d07317608f92d34b0089d5f0e82402b61f8c
ZiniuZheng <117133246+ZiniuZheng@users.noreply.github.com>
2024-07-15 16:21:16 +0800

    Update DateDemo.java

--- DateDemo.java
+++ DateDemo.java
@@ -1,6 +1,12 @@
 package homework;
 
 import java.util.Date;
+/*
+ * 需求：
+ * 		1.通过Date对象获取当前时间的毫秒值
+ * 		2.计算出明天这个时间的毫秒值
+ * 		3.将明天这个时间的毫秒值转换成Date对象
+ */
 public class DateDemo {
 
 	public static void main(String[] args) {
